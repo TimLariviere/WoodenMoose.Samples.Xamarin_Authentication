9e9f4c34bd15d8b962b7bfa24fc3e47330283b8a
Timothé Larivière <tlariviere@mcnext.com>
2017-06-19 09:12:41 +0200

    Updated Configuration

--- src/AuthenticationSample/Configuration.cs
+++ src/AuthenticationSample/Configuration.cs
@@ -4,7 +4,7 @@
     {
         public const string ClientId = "150526ad-75ee-44d5-a6e1-091ef0900d77"; // Put your mobile app ClientId
         public const string Authority = "https://login.windows.net/mcnext.com/"; // Default authority for Azure AD
-        public const string Resource = "https://woodenmoose-authsample-api.azurewebsites.net"; // Put your API AppId
+        public const string Resource = "c21a38b2-54bb-4027-b5e4-257f7d10f3f9"; // Put your API ClientId
         public const string RedirectUri = "https://mcnext.com/woodenmoose-authsample-mobileapp"; // Put your mobile app Redirect Uri (declared in Azure AD Apps)
         public const string ApiUri = "https://woodenmoose-authsample-api.azurewebsites.net/api/"; // Put you API actual URL
     }
